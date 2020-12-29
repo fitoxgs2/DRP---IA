89e5aa1a1a58afc08dac9986f0dfa5ebef9fdb9e
Fito <rodolfo.jaramillo.14@sansano.usm.cl>
2020-12-29 00:08:34 -0300

    Mejor mejora
    Funciona la mejor mejora para el caso de los AED fixed location problem

--- IA.cpp
+++ IA.cpp
@@ -43,11 +43,50 @@ int sumatoria(vector<bool> lista){
     return suma;
 }
 
-//Actualiza la lista de OHCA cubiertos cada vez que se agrega un AED.
-void actualizar_cubiertos(int aed, unsigned n_size){
-    for (int i=0; i<n_size; i++){
-        if (radios_cubiertos[aed][i]){
-            cubiertos[i] = 1;
+//Actualiza la lista de OHCA cubiertos
+void actualizar_cubiertos(unsigned n_size){
+    int aed,ohca;
+    for(aed = 0; aed < n_size; aed++){
+        if (ubicados[aed]){
+            for(ohca = 0; ohca < n_size; ohca++){
+                if (radios_cubiertos[aed][ohca]){
+                    cubiertos[ohca] = 1;
+                }
+            }
+        }
+    }
+}
+
+//Evaluacion del movimiento SWAP entre dos AED
+//Se cambia uno ubicado por otro que no
+//Entrega la cantidad de nodos diferentes que serian cubiertos
+//Positivo si es que se cubren mas que antes, 0 si no cambia y negativo si se cubren menos
+int funcion_eval(int aed1, int aed2, unsigned n_size){
+    vector<bool> save_state = cubiertos;
+    int valor_original = sumatoria(cubiertos);
+    //Swap
+    ubicados[aed1] = 0;
+    ubicados[aed2] = 1;
+    actualizar_cubiertos(n_size);
+    int valor_nuevo = sumatoria(cubiertos);
+    //Deshacer swap
+    ubicados[aed1] = 1;
+    ubicados[aed2] = 0;
+    cubiertos = save_state;
+    return valor_nuevo-valor_original;
+}
+
+//MM es un array que contiene los indices de los 2 AED a cambiar y el valor de su funcion
+void vecindario_swap(int aed, int MM[], unsigned n_size){
+    int actual_value;
+    for(int i=0; i<n_size; i++){
+        if(!ubicados[i]){
+            actual_value = funcion_eval(aed, i, n_size);
+            if (actual_value > MM[2]){
+                MM[0] = aed;
+                MM[1] = i;
+                MM[2] = actual_value;
+            }
         }
     }
 }
@@ -69,7 +108,7 @@ float greedy(float budget, unsigned n_size){
     }
     gastos = gastos + 1;
     ubicados[primer_aed] = 1;
-    actualizar_cubiertos(primer_aed, n_size);
+    actualizar_cubiertos(n_size);
     
     while(gastos<budget){
         best_index = -1;
@@ -84,7 +123,7 @@ float greedy(float budget, unsigned n_size){
         }
         gastos = gastos + 1;
         ubicados[best_index] = 1;
-        actualizar_cubiertos(best_index, n_size);
+        actualizar_cubiertos(n_size);
     }
     return gastos;
 }
@@ -97,6 +136,7 @@ int main(int argc, char *argv[])
     unsigned N, R; //Numero de eventos y radio
     int x, y, c; //Variables que tomaran los valores de cada linea en la instancia
     unsigned i,j; //iteradores
+    int MM[3]; //Mejor mejora. Contiene AED1, AED2 y valor de la funcion evaluacion.
     
     string line;
     ifstream instancia (argv[1]);
@@ -122,8 +162,29 @@ int main(int argc, char *argv[])
             }
         }
     }
+    //Algoritmo greedy para obtener solucion inicial
     gastos = greedy(B,N);
-    
+
+    //Hill climbing (se detiene al llegar a la cima)
+    while(true){
+        //Se comienza a iterar en el vecindario para obtener el mejor swap posible
+        MM[0] = -1;
+        MM[1] = -1;
+        MM[2] = -1;
+
+        for (i=0; i<N; i++){
+            if(ubicados[i]){
+                vecindario_swap(i, MM, N);
+            }
+        }
+        if(MM[2] >= 0){
+            ubicados[MM[0]] = 0;
+            ubicados[MM[1]] = 1;
+            actualizar_cubiertos(N);
+        } else {
+            break;
+        }
+    }
     clock_t fin = clock();
     tiempo_ejecucion += (double)(fin-inicio)/CLOCKS_PER_SEC;
     cout << "Numero de eventos cubiertos: " << sumatoria(cubiertos) <<endl;
